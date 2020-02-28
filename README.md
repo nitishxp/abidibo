5cf28850183eed4b33dd0f124ac7fc77ed752791
nitish.rai <nitish.rai@globallogic.com>
2020-02-28 12:12:34 +0530

    ADded

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# abidibo" 
+
+To install postgres
+
+docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=chef -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data  postgres
--- app/admin.py
+++ app/admin.py
@@ -1,17 +1,21 @@
+from imagekit.admin import AdminThumbnail
+from django.contrib.contenttypes.admin import (
+    GenericTabularInline, GenericStackedInline, GenericInlineModelAdmin,)
 from django.contrib import admin
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 from app.models import (GradeModel, LearningObjective,
                         CountryModel, QuestionModel,)
 
+from django.contrib.auth.models import User, Group
 
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 # from common.forms import (RecipeStepModelForm, RecipeModelForm,)
-from django.contrib.contenttypes.admin import (
-    GenericTabularInline, GenericStackedInline, GenericInlineModelAdmin,)
-from imagekit.admin import AdminThumbnail
 
 
-class QuestionModelInline(GenericTabularInline):
+class QuestionModelInline(GenericTabularInline, DynamicArrayMixin):
     model = QuestionModel
     extra = 0
     ct_field_name = 'content_type'
